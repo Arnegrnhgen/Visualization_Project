1e19f5f5d7cb1369a9f0efb130d658b51afb1801
Arne Grünhagen <arne.gruenhagen@desy.de>
2024-02-20 13:43:56 +0100

    added dashboard

--- dashboard.py
+++ dashboard.py
@@ -12,11 +12,12 @@ actual plotly-resampler graph and cache it on the server side. These two callbac
 chained together using the dcc.Interval component.
 
 """
+from data import get_doocs_properties, load_parquet_data
+from pathlib import Path
+from datetime import datetime
 
 from typing import List
-from uuid import uuid4
 
-import numpy as np
 import plotly.graph_objects as go
 from dash import MATCH, Input, Output, State, dcc, html, no_update
 from dash_extensions.enrich import (
@@ -32,72 +33,90 @@ from plotly_resampler import FigureResampler
 from plotly_resampler.aggregation import MinMaxLTTB
 
 # Data that will be used for the plotly-resampler figures
-x = np.arange(2_000_000)
-noisy_sin = (3 + np.sin(x / 200) + np.random.randn(len(x)) / 10) * x / 1_000
+online_data = {}
+
+doocs_properties = get_doocs_properties(Path("C:/Users/Arne/Sources/Ml_data/daqdata/sorted"))
+
 
 # --------------------------------------Globals ---------------------------------------
 app = DashProxy(__name__, transforms=[ServersideOutputTransform(), TriggerTransform()])
 
+
 app.layout = html.Div(
     [
-        html.Div(children=[html.Button("Add Chart", id="add-chart", n_clicks=0)]),
+        html.Div(children=[
+            dcc.Dropdown(doocs_properties, id="property-selecter", multi=True),
+            html.Button("load data and plot", id="load-plot")
+        ]),
+
         html.Div(id="container", children=[]),
     ]
 )
 
 
 # ------------------------------------ DASH logic -------------------------------------
-# This method adds the needed components to the front-end, but does not yet contain the
-# FigureResampler graph construction logic.
 @app.callback(
     Output("container", "children"),
-    Input("add-chart", "n_clicks"),
+    Input("load-plot", "n_clicks"),
+    State("property-selecter", "value"),
     State("container", "children"),
     prevent_initial_call=True,
 )
-def add_graph_div(n_clicks: int, div_children: List[html.Div]):
-    print("hi")
-    uid = str(uuid4())
-    new_child = html.Div(
-        children=[
-            # The graph and its needed components to serialize and update efficiently
-            # Note: we also add a dcc.Store component, which will be used to link the
-            #       server side cached FigureResampler object
-            dcc.Graph(id={"type": "dynamic-graph", "index": uid}, figure=go.Figure()),
-            dcc.Loading(dcc.Store(id={"type": "store", "index": uid})),
-            TraceUpdater(id={"type": "dynamic-updater", "index": uid}, gdID=f"{uid}"),
-            # This dcc.Interval components makes sure that the `construct_display_graph`
-            # callback is fired once after these components are added to the session
-            # its front-end
-            dcc.Interval(
-                id={"type": "interval", "index": uid}, max_intervals=1, interval=1
-            ),
-        ],
-    )
-    div_children.append(new_child)
-    return div_children
+def add_graph_div(_, selected_properties, div_children: List[html.Div] ):
+    if selected_properties is None:
+        return []
+    else:
+        props = [Path(prop) for prop in selected_properties]
+        loaded_data = load_parquet_data(props, datetime(2023, 10, 15, 17, 30), datetime(2023, 11, 15, 17, 30))
+        for key, item in loaded_data.items():
+            online_data[doocs_properties[str(key)]] = item
+
+        div_children = []
+        #### make the plots here
+        for dat in loaded_data:
+            uid = doocs_properties[str(dat)]
+            new_child = html.Div(
+                children=[
+                    # The graph and its needed components to serialize and update efficiently
+                    # Note: we also add a dcc.Store component, which will be used to link the
+                    #       server side cached FigureResampler object
+                    dcc.Graph(id={"type": "dynamic-graph", "index": uid}, figure=go.Figure()),
+                    dcc.Loading(dcc.Store(id={"type": "store", "index": uid})),
+                    TraceUpdater(id={"type": "dynamic-updater", "index": uid}, gdID=f"{uid}"),
+                    # This dcc.Interval components makes sure that the `construct_display_graph`
+                    # callback is fired once after these components are added to the session
+                    # its front-end
+                    dcc.Interval(
+                        id={"type": "interval", "index": uid}, max_intervals=1, interval=1
+                    ),
+                ],
+            )
+
+            div_children.append(new_child)
+        return div_children
 
 
 # This method constructs the FigureResampler graph and caches it on the server side
 @app.callback(
     Output({"type": "dynamic-graph", "index": MATCH}, "figure"),
     Output({"type": "store", "index": MATCH}, "data"),
-    State("add-chart", "n_clicks"),
+    State("load-plot", "n_clicks"),
+    State({"type": "dynamic-graph", "index": MATCH}, "id"),
     Trigger({"type": "interval", "index": MATCH}, "n_intervals"),
     prevent_initial_call=True,
 )
-def construct_display_graph(n_clicks) -> FigureResampler:
+def construct_display_graph(n_clicks, analysis) -> FigureResampler:
     fig = FigureResampler(
         go.Figure(),
         default_n_shown_samples=2_000,
         default_downsampler=MinMaxLTTB(parallel=True),
     )
-    print(n_clicks)
-    # Figure construction logic based on a state variable, in our case n_clicks
+
+    data = online_data[analysis['index']]
+
     sigma = n_clicks * 1e-6
-    fig.add_trace(dict(name="log"), hf_x=x, hf_y=noisy_sin * (1 - sigma) ** x)
-    fig.add_trace(dict(name="exp"), hf_x=x, hf_y=noisy_sin * (1 + sigma) ** x)
-    fig.update_layout(title=f"<b>graph - {n_clicks}</b>", title_x=0.5)
+    fig.add_trace(dict(name="new"), hf_x=data["timestamp"], hf_y=data["data"])
+    fig.update_layout(title=f"<b>{analysis['index']}</b>", title_x=0.5)
 
     return fig, Serverside(fig)
 
